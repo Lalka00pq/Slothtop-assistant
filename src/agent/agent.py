d02b22db34169328bf2af8128c270589154a3608
Lalka00pq <lalka00pq@gmail.com>
2025-06-24 09:45:05 +0300

    correct comment formatting and tidy up tool list

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -6,7 +6,7 @@ from src.tools.tools import turn_of_pc, reload_pc, open_app, close_app
 from smolagents import HfApiModel, CodeAgent, DuckDuckGoSearchTool
 from huggingface_hub import login
 from dotenv import load_dotenv
-## Login to Hugging Face
+# Login to Hugging Face
 load_dotenv()
 api_key = os.getenv("HF_API_KEY")
 login(token=api_key)
@@ -16,5 +16,5 @@ tools = [turn_of_pc,
          close_app,
          reload_pc,
          DuckDuckGoSearchTool(),
-]
+         ]
 agent = CodeAgent(tools=tools, model=HfApiModel())
