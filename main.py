d5f8ad3f7fb74f109dc24c96b416a8af0365b56d
Lalka00pq <lalka00pq@gmail.com>
2025-08-22 16:53:57 +0300

    Changed main file

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from src.app.app import app_page
 import flet as ft
 
 
-# def main() -> None:
-#     """Main function to create the agent and invoke it with a sample input."""
-#     ft.app(target=app_page, view=ft.AppView.FLET_APP)
+def main() -> None:
+    """Main function to create the agent and invoke it with a sample input."""
+    ft.app(target=app_page, view=ft.AppView.FLET_APP)
 
-ft.app(target=app_page, view=ft.AppView.FLET_APP)
-# if __name__ == "__main__":
-#     main()
+
+if __name__ == "__main__":
+    main()
